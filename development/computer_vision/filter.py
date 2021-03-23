812f46db899107ac6e22e6f6b30d7ab42c51030e
suyiwen98 <suyiwen1@gmail.com>
2021-03-23 21:06:01 +0100

    Update filter.py

--- development/computer_vision/filter.py
+++ development/computer_vision/filter.py
@@ -6,11 +6,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import glob
 import re
-
+import random
 import calibration
 
 
-def filter_color(image_name, y_low, y_high, u_low, u_high, v_low, v_high, resize_factor):
+def filter_color(image_name, y_low, y_high, cb_low, cb_high, cr_low, cr_high, resize_factor):
+    """This creates a YCrCb based image filter and returns a binary image that shows
+    the pexels within the threshold in white and the other ones in black"""
     original = calibration.undistort(image_name)
     #blur the image to remove noise
     im = cv2.blur(original, (10, 10))
@@ -26,13 +28,12 @@ def filter_color(image_name, y_low, y_high, u_low, u_high, v_low, v_high, resize
     for y in range(YCrCb.shape[0]):
         for x in range(YCrCb.shape[1]):
             if (y_low <= YCrCb[y, x, 0] <= y_high and
-                    u_low <= YCrCb[y, x, 1] <= u_high and
-                    v_low <= YCrCb[y, x, 2] <= v_high):
+                    cb_low <= YCrCb[y, x, 1] <= cb_high and
+                    cr_low <= YCrCb[y, x, 2] <= cr_high):
                 Filtered[y, x] = 1
     
     ret, Filtered = cv2.threshold(Filtered, 0, 255, cv2.THRESH_BINARY)
     
-
     plt.figure();
     RGB = cv2.cvtColor(original, cv2.COLOR_BGR2RGB);
     plt.imshow(RGB);
@@ -50,6 +51,7 @@ if __name__ == '__main__':
     image_dir_path = './AE4317_2019_datasets/cyberzoo_poles_panels_mats/20190121-142935/*.jpg'
     filenames = glob.glob(image_dir_path)
     filenames.sort(key=lambda f: int(re.sub('\D', '', f)))
-    for i in filenames[0:10]:
-        Filtered_pole = filter_color(i, y_low = 50, y_high = 255, u_low = 0, 
-                                 u_high = 130, v_low = 0, v_high = 140, resize_factor=1)
+    #check if the filter works on the randomly selected image
+    n=random.randint(0,len(filenames))
+    Filtered = filter_color(filenames[n], y_low = 50, y_high = 255, cb_low = 0, 
+                             cb_high = 130, cr_low = 0, cr_high = 135, resize_factor=1)
